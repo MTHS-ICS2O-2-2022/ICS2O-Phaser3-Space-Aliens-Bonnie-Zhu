f076931764f872663330858f465abf55fab63022
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-05-11 17:13:22 +0000

    3-10

--- js/gameScene.js
+++ js/gameScene.js
@@ -7,72 +7,86 @@
 
 /**
  * This class is the Game scene
- * 
+ *
  */
 class GameScene extends Phaser.Scene {
   /**
-   * This method is the consructor
+   * This method is the constructor.
    */
-  constructor () {
-    super({ key: 'gameScene' })
-
+  constructor() {
+    super({ key: "gameScene" })
     this.background = null
     this.ship = null
+    this.missilesGroup = null // Added declaration for missilesGroup
+    this.fireMissile = false // Added declaration for fireMissile
   }
 
   /**
    * Can be defined on your own Scenes.
    * This method is called by the Scene Manager when the scene starts,
-   * before preload() and create()
+   * before preload() and create().
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
-  init (data) {
-    this.cameras.main.setBackgroundColor('ffffff')
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
   }
 
   /**
    * Can be defined on your own Scenes.
    * Use it to load assets.
    */
-  preload () {
-    console.log('Game Scene')
-
-    this.load.image('starBackground', 'assets/starBackground.png')
-    this.load.image('ship', 'assets/spaceShip.png')
+  preload() {
+    console.log("Game Scene")
+    this.load.image("starBackground", "assets/starBackground.png")
+    this.load.image("ship", "assets/spaceShip.png")
+    this.load.image("missile", "assets/missile.png") // Added missile asset
   }
 
   /**
    * Can be defined on your own Scenes.
    * Use it to create your game objects.
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
-  create (data) {
-    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+  create(data) {
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+
+    this.missilesGroup = this.physics.add.group() // Moved missilesGroup creation to create() method
   }
 
   /**
-   * should be overridden by your own Scenes.
+   * Should be overridden by your own Scenes.
    * This method is called once per game step while the scene is running.
    * @param {number} time - The current time.
-   * @param {number} delta - The delta time in ms since the last time.
+   * @param {number} delta - The delta time in ms since the last frame.
    */
-  update (time, delta) {
-    const keyLeftObj = this.input.keyboard.addKey('LEFT')
-    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+  update(time, delta) {
+    const keyLeftObj = this.input.keyboard.addKey("LEFT")
+    const keyRightObj = this.input.keyboard.addKey("RIGHT")
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
     if (keyLeftObj.isDown === true) {
-      this.ship.x -=15
-      if (this.ship.x < 0) {
-        this.ship.x = 0
-      }
+      this.ship.x -= 15
+      if (this.ship.x < 0) this.ship.x = 0
     }
+
     if (keyRightObj.isDown === true) {
-      this.ship.x +=15
-      if (this.ship.x > 1920) {
-        this.ship.x = 1920
+      this.ship.x += 15
+      if (this.ship.x > 1920) this.ship.x = 1920
+    }
+
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        // fire a missile
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(
+          this.ship.x,
+          this.ship.y,
+          "missile"
+        )
+        this.missilesGroup.add(aNewMissile)
       }
     }
   }
