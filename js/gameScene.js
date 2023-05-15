30869263ebd3004de1184c5c3db0d12c8ef01a42
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-05-15 17:01:16 +0000

    done

--- js/gameScene.js
+++ js/gameScene.js
@@ -17,64 +17,52 @@ class GameScene extends Phaser.Scene {
     super({ key: "gameScene" })
     this.background = null
     this.ship = null
-    this.missilesGroup = null // Added declaration for missilesGroup
     this.fireMissile = false // Added declaration for fireMissile
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * This method is called by the Scene Manager when the scene starts,
-   * before preload() and create().
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to load assets.
-   */
   preload() {
     console.log("Game Scene")
+
+    // images
     this.load.image("starBackground", "assets/starBackground.png")
     this.load.image("ship", "assets/spaceShip.png")
-    this.load.image("missile", "assets/missile.png") // Added missile asset
+    this.load.image("missile", "assets/missile.png")
+    // sounds
+    this.load.audio("laser", "assets/laser1.wav")
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to create your game objects.
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   create(data) {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
-    this.missilesGroup = this.physics.add.group() // Moved missilesGroup creation to create() method
+    //create missilesGroup` empty container
+    this.missilesGroup = this.physics.add.group()
   }
 
-  /**
-   * Should be overridden by your own Scenes.
-   * This method is called once per game step while the scene is running.
-   * @param {number} time - The current time.
-   * @param {number} delta - The delta time in ms since the last frame.
-   */
   update(time, delta) {
+    //called 0 times a second, hopefully
     const keyLeftObj = this.input.keyboard.addKey("LEFT")
     const keyRightObj = this.input.keyboard.addKey("RIGHT")
     const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
-      if (this.ship.x < 0) this.ship.x = 0
+      if (this.ship.x < 0) {
+        this.ship.x = 0
+      }
     }
 
     if (keyRightObj.isDown === true) {
       this.ship.x += 15
-      if (this.ship.x > 1920) this.ship.x = 1920
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
     }
 
     if (keySpaceObj.isDown === true) {
@@ -86,9 +74,20 @@ class GameScene extends Phaser.Scene {
           this.ship.y,
           "missile"
         )
-        this.missilesGroup.add(aNewMissile)
+        this.missilesGroup, add(aNewMissile)
+        this.sound, play("laser")
       }
     }
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false
+    }
+
+    this.missilesGroup.children.each(function (item) {
+      item.y = item.y - 15
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
   }
 }
 
